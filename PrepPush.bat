9728984bb51bf707c0692ab2c3d1b45d9f072a83
= <=jm726@st-andrews.ac.uk>
2013-02-08 16:21:01 +0000

    Removed unnecessary lines from script.

--- PrepPush.bat
+++ PrepPush.bat
@@ -14,10 +14,6 @@ cd %workingDir%
 
 Echo Updating source files
 
-set files=%src%llappviewer.cpp %src%llappviewer.h %src%llstartup.cpp %src%llstartup.h %src%llviewercamera.cpp %src%llviewercamera.h %src%llviewermessage.cpp %src%llviewermessage.h %src%llviewernui.cpp %src%llviewernui.h scripts\messages\message_template.msg %src%\app_settings\settings.xml 
-
-echo %files%
-
 git checkout FirestormHead
 xcopy /d /y %original%%src%*.cpp Source%src%
 xcopy /d /y %original%%src%*.h Source%src%
@@ -38,10 +34,6 @@ git checkout %head%
 for /f "delims=" %%l in ('git diff --name-only FirestormHead WorkingHead') do (
 	call:subroutine "%%l"
 )
-REM call:diffFolder ..%src% *.cpp
-REM call:diffFolder ..%src% *.h
-REM call:diffFolder ..%settings% *.xml
-REM call:diffFolder ..%messages% *.msg
 
 xcopy /s /c /d /e /h /i /r /y /exclude:.copyignore ..\build-vc100\newview\Release\* Bin\
 xcopy /s /c /d /e /h /i /r /y /exclude:.copyignore ..%messages%message_template.msg ..%settings%
@@ -52,19 +44,4 @@ goto:eof
 :subroutine
 	git checkout WorkingHead %1
 	git diff FirestormHead WorkingHead %1 > Diff\%~xn1.diff
-goto:eof
-
-:diffFolder
-	for /f "delims=" %%f in ('dir %1%2 /b') do (
-		git diff FirestormHead WorkingHead Source\%%f > Diff\%%f.diff
-		call:sizeDel Diff\%%f.diff
-	)
-goto:eof
-
-:sizeDel
-if %~z1 == 0 (
-	Del %1
-) else (
-	git checkout -f WorkingHead Source\%~n1
-)
 goto:eof
