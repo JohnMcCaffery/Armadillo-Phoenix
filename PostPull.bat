1b35f7d4c4423786e16c2d903a9ad28421bca14a
= <=jm726@st-andrews.ac.uk>
2013-02-25 12:40:01 +0000

    Fixed PostPull.bat

--- PostPull.bat
+++ PostPull.bat
@@ -5,7 +5,7 @@ Echo Updating source files
 
 xcopy /d /y Source\indra\newview\*.cpp ..\indra\newview
 xcopy /d /y Source\indra\newview\*.h ..\indra\newview\
-xcopy /d /y Source\app_settings\*.xml ..\indra\newview\app_settings\
-xcopy /d /y Source\messages\*.msg ..\scripts\messages\
+xcopy /d /y Source\indra\newview\app_settings\*.xml ..\indra\newview\app_settings
+xcopy /d /y Source\scripts\messages\*.msg ..\scripts\messages\
 
 goto:eof
