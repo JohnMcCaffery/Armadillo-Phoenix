bd2da90a0b7db398c7e0cc1c0c9406f5ebb44c2d
= <=jm726@st-andrews.ac.uk>
2013-02-25 16:28:37 +0000

    Improved how pull and push scripts deal with message template.

--- PostPull.bat
+++ PostPull.bat
@@ -3,9 +3,10 @@ set src=\indra\newview\
 
 Echo Updating source files
 
-xcopy /d /y Source\indra\newview\*.cpp ..\indra\newview
-xcopy /d /y Source\indra\newview\*.h ..\indra\newview\
-xcopy /d /y Source\indra\newview\app_settings\*.xml ..\indra\newview\app_settings
-xcopy /d /y Source\scripts\messages\*.msg ..\scripts\messages\
+xcopy /y Source\indra\newview\*.cpp ..\indra\newview
+xcopy /y Source\indra\newview\*.h ..\indra\newview\
+xcopy /y Source\indra\newview\app_settings\*.xml ..\indra\newview\app_settings
+xcopy /y Source\scripts\messages\*.msg ..\scripts\messages\
+xcopy /y Source\scripts\messages\*.msg ..\build-vc100\newview\Release\app_settings\
 
 goto:eof
