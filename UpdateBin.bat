6233aeb19e1cfdee082066812a218ae1e3f5cf89
= <=jm726@st-andrews.ac.uk>
2013-04-04 18:17:57 +0100

    Saving state before pulling changes.

--- UpdateBin.bat
+++ UpdateBin.bat
@@ -1,2 +1,2 @@
 xcopy /s /c /d /e /h /i /r /y /exclude:.copyignore ..\build-vc100\newview\Release\* Bin\
-xcopy /s /c /d /e /h /i /r /y /exclude:.copyignore ..%src%* Bin\
+xcopy /s /c /d /e /h /i /r /y /exclude:.copyignore ..\indra\newview\* Bin\
