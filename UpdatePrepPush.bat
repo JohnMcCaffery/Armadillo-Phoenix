74536b180bb1f684fef96134ea534209924f41ba
= <=jm726@st-andrews.ac.uk>
2013-04-04 17:18:28 +0100

    Updated UpdatePrepPush to only commit changed PrepPush file.

--- UpdatePrepPush.bat
+++ UpdatePrepPush.bat
@@ -6,12 +6,12 @@ git commit -m "About to update PrepPush"
 
 git checkout FirestormHead
 git checkout %head% PrepPush.bat
-git add .
+git add PrepPush.bat
 git commit -m "Updated PrepPush."
 
 git checkout WorkingHead
 git checkout %head% PrepPush.bat
-git add .
+git add PrepPush.bat
 git commit -m "Updated PrepPush."
 
 git checkout %head%
