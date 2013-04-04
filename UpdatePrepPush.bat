023aad36ef119dccc37a574535acfac21a7f0d4b
= <=jm726@st-andrews.ac.uk>
2013-04-04 15:00:51 +0100

    Added UpdatePrepPush. Forced PrepPush to commit all changes before pulling.

---
+++ UpdatePrepPush.bat
@@ -0,0 +1,17 @@
+for /f "delims=" %%a in ('git symbolic-ref HEAD') do set head=%%a
+set head=%head:~11
+
+git add .
+git commit -m "About to update PrepPush"
+
+git checkout FirestormHead
+git checkout %head% PrepPush.bat
+git add .
+git commit -m "Updated PrepPush."
+
+git checkout FirestormHead
+git checkout %head% PrepPush.bat
+git add .
+git commit -m "Updated PrepPush."
+
+git checkout %head%
